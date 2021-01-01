0b70808a81f59586981d71cc5568daaf71ec1928
BertKUL <bertclybouw@telenet.be>
2021-01-01 18:47:04 +0100

    Update ApiKeyAuthentication.php

--- src/Authentication/ApiKeyAuthentication.php
+++ src/Authentication/ApiKeyAuthentication.php
@@ -9,11 +9,13 @@ class ApiKeyAuthentication implements Authentication
 {
     private $apiKey;
     private $apiSecret;
+    private $passphrase;
 
-    public function __construct($apiKey, $apiSecret)
+    public function __construct($apiKey, $apiSecret, $passphrase)
     {
         $this->apiKey = $apiKey;
         $this->apiSecret = $apiSecret;
+        $this->passphrase = $passphrase;
     }
 
     public function getApiKey()
@@ -24,6 +26,15 @@ class ApiKeyAuthentication implements Authentication
     public function setApiKey($apiKey)
     {
         $this->apiKey = $apiKey;
+    }
+    public function getPassphrase()
+    {
+        return $this->passphrase;
+    }
+
+    public function setPassphrase($passphrase)
+    {
+        $this->passphrase = $passphrase;
     }
 
     public function getApiSecret()
@@ -39,13 +50,23 @@ class ApiKeyAuthentication implements Authentication
     public function getRequestHeaders($method, $path, $body)
     {
         $timestamp = $this->getTimestamp();
-        $signature = $this->getHash('sha256', $timestamp.$method.$path.$body, $this->apiSecret);
-
+//        $signature = $this->getHash('sha256', $timestamp.$method.$path.$body, $this->apiSecret);
+        $signature = $this->signature($path, $body, $timestamp, $method);
         return [
             'CB-ACCESS-KEY'       => $this->apiKey,
             'CB-ACCESS-SIGN'      => $signature,
             'CB-ACCESS-TIMESTAMP' => $timestamp,
+            'CB-ACCESS-PASSPHRASE' => $this->passphrase,
         ];
+    }
+
+    public function signature($request_path='', $body='', $timestamp=false, $method='GET') {
+        $body = is_array($body) ? json_encode($body) : $body;
+        $timestamp = $timestamp ? $timestamp : time();
+
+        $what = $timestamp.$method.$request_path.$body;
+
+        return base64_encode(hash_hmac("sha256", $what, base64_decode($this->apiSecret), true));
     }
 
     public function createRefreshRequest($baseUrl)
